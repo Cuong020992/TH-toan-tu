83331c589ec87c8c29df843f35f9196649ba004b
cuong0209992 <sandtimezone92@gmail.com>
2021-03-09 13:45:00 +0700

    Bài thực hành sử dụng toán tử

---
+++ SD-toan-tu.js
@@ -0,0 +1,9 @@
+let input_height = prompt("nhập giá trị chiều cao: ");
+let input_width = prompt("nhập giá trị chiều rộng: ");
+
+let height = parseInt(input_height);
+let width = parseInt(input_width);
+
+let area = height * width;
+
+document.write("Diện tích hình chữ nhật là: "+area);
